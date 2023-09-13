8bb4a3a48c2700a778e483acd72d9019a34fc694
dantaeil <danjohnson0037@gmail.com>
2023-09-13 07:15:58 -0400

    feat:Banner1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Hero from "./components/hero/Hero";
 import Post from "./components/post/Post";
+import Banner1 from "./components/banners/banner1/Banner1";
 
 function App() {
   return (
     <div>
       <Hero />
       <Post />
+      <Banner1 />
     </div>
   );
 }
---
+++ src/components/banners/banner1/Banner1.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Banner1.scss";
+
+const Banner1 = () => {
+  return (
+    <div className="container-fluid banner1">
+      <div className="row">
+        <div className="col bannertitle">LOREM IPSUM</div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="bannerbutton">BUTTON BUTTON</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner1;
