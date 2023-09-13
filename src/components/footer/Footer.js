b7362523d38e31eb4b9dfa4871cd56b9b982ca97
dantaeil <danjohnson0037@gmail.com>
2023-09-13 09:47:43 -0400

    fix:Footer Links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className="container footer">
       <div className="row justify-content-center">
-        <div className="col-md-auto align-self-center">Follow Us:</div>
+        <div className="col-md-auto align-self-center followus">Follow Us:</div>
       </div>
       <div className="row justify-content-center">
         <div className="col-md-auto align-self-center">
